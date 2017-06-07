770e9fa23f4152a15994bd02aaa7146c597f25a4
Spencer Hamilton <spencerjhamilton@gmail.com>
2017-06-07 02:27:54 +0000

    Vol 6 - Updated ActiveRecord #bigchanges

--- app/actions.rb
+++ app/actions.rb
@@ -7,47 +7,6 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-    @post_bass = {
-        username: "ItsMeFish",
-        avatar_url: "http://kristiemercer.com/wp-content/uploads/2014/04/fish.gif",
-        photo_url: "http://www.taiwilliams.com/blog/wp-content/uploads/2014/08/tai-spike-august03-09.jpg",
-        humanized_time_ago: humanized_time_ago(10),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "ItsMeFish",
-            text: "Just got back from school lol"
-        }]
-    }
-   
-    @post_catfish = {
-        username: "Whiskers4Days",
-        avatar_url: "http://orig13.deviantart.net/cdcb/f/2012/176/a/b/trippy_fish_by_sean83421-d54rut3.jpg",
-        photo_url: "https://1source.basspro.com/images/Catfish%20in%20dark%20waters.jpg",
-        humanized_time_ago: humanized_time_ago(4),
-        like_count: 2,
-        comment_count: 1,
-        comments: [{
-            username: "Whiskers4Days",
-            text: "Suns out guns out"
-        }]
-    }
-    
-     @post_drawings = {
-        username: "HopelessFlounderer",
-        avatar_url: "http://i58.tinypic.com/2it3hhg.jpg",
-        photo_url: "http://www.how-to-draw-cartoons-online.com/image-files/cartoon-catfish-6.gif",
-        humanized_time_ago: humanized_time_ago(10),
-        like_count: 134,
-        comment_count: 1,
-        comments: [{
-            username: "HopelessFlounderer",
-            text: "Credit to ArtFish, looks just like me #smile"
-        }]
-    }
-    
-    @posts = [@post_bass, @post_catfish, @post_drawings]
-
-erb(:index)
-
+    @posts = Post.order(created_at: :desc)
+    erb(:index)
 end
