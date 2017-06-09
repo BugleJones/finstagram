4e61759b185671a7e27a28d05a1adb9882b999b2
Spencer Hamilton <spencerjhamilton@gmail.com>
2017-06-09 02:54:00 +0000

    Vol 7 - Added Forms now have signup page

--- app/actions.rb
+++ app/actions.rb
@@ -10,3 +10,30 @@ get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
 end
+
+get "/signup" do     # if a user navigates to the path "/signup"
+    @user = User.new # setup empty @user object
+    erb(:signup)     # render "app/views/signups.erb"
+end
+
+post "/signup" do
+    
+    # grab user input values from params
+    email      = params[:email]
+    avatar_url = params[:avatar_url]
+    username   = params[:username]
+    password   = params[:password]
+    
+    # instantiate a user
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    # if user validations pass and user is saved
+    if @user.save
+        # return readable representation of the object
+        "User #{username} saved!"
+    else
+        #display error messages
+        erb(:signup)
+    end
+end
+
+#require Are you a fish checkbox?
