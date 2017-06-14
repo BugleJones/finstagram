550b6cdf39ccc2333df7ab55d13c44961b8de894
Spencer Hamilton <spencerjhamilton@gmail.com>
2017-06-14 00:03:30 +0000

    Vol 8 - Added in Signup pages and linked pages together

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do 
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 def humanized_time_ago(time_ago_in_minutes)
     if time_ago_in_minutes >= 60
         "#{time_ago_in_minutes / 60} hours ago"
@@ -12,8 +18,19 @@ get '/' do
 end
 
 get "/signup" do     # if a user navigates to the path "/signup"
-    @user = User.new # setup empty @user object
-    erb(:signup)     # render "app/views/signups.erb"
+    email      = params[:email]
+    avatar_url = params[:avatar_url]
+    username   = params[:username]
+    password   = params[:password]
+    
+
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    if @user.save
+        redirect to("/login")
+    else
+        erb(:signup)
+    end
 end
 
 post "/signup" do
@@ -34,6 +51,34 @@ post "/signup" do
         #display error messages
         erb(:signup)
     end
+end
+
+get "/login" do  # when a GET request comes into /login
+
+    erb(:login)
+    
+end
+
+post "/login" do
+    username = params[:username] # username retrieve
+    password = params[:password] # password retrieve
+    
+    # 1. find by username
+    user = User.find_by(username: username)
+    
+    # 2. if that user exists
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to("/")
+    else
+        @error_message = "No fish found."
+        erb(:login)
+    end
+end
+
+get "/logout" do
+    session[:user_id] = nil
+    redirect to("/")
 end
 
 #require Are you a fish checkbox?
